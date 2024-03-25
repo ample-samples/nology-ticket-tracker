e75536303572aef22e52a46b88ef9b28bec47898
Todd <todd.griffin61@outlook.com>
2024-03-25 10:12:43 +0000

    implemented counter state and handler functions

--- src/components/Ticket.tsx
+++ src/components/Ticket.tsx
@@ -1,9 +1,26 @@
 import "./Ticket.scss"
+import { useState } from "react"
 
 export const Ticket = () => {
+  const [ counter, setCounter ] = useState(0)
+  const handleIncrement = () => setCounter(counter + 1)
+  const handleDecrement = () => {
+    if (counter > 0) {
+      setCounter(counter - 1)
+    }
+  }
   return (
-  <div className="ticket">
-      <h3>Ticket</h3>
+    <div className="ticket">
+      <h3 className="ticket__heading ticket__heading--name">name</h3>
+      <h3 className="ticket__heading ticket__heading--role">role</h3>
+      <section className="ticket__counter">
+        <h4 className="ticket__counter-heading">Counter</h4>
+        <p>{counter}</p>
+        <div className="ticket__counter-controls">
+          <button className="ticket__counter-decrement" onClick={handleDecrement}>-</button>
+          <button className="ticket__counter-increment" onClick={handleIncrement}>+</button>
+        </div>
+      </section>
     </div>
-)
+  )
 }
