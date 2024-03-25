3ee7f3b0bcd7b54d5875b45639c75762f9029fc2
Todd <todd.griffin61@outlook.com>
2024-03-25 10:49:23 +0000

    styled tickets

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Ticket } from "./components/Ticket"
 import "./app.scss"
+import team from "./data/team"
 
 function App() {
   return (
     <section id="app">
-      <Ticket name="Todd" role="software engineer" />
+      {team.map(({ name, role, id }) =>  <Ticket name={name} role={role} key={id} /> )}
     </section>
   )
 }
