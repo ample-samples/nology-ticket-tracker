d000ad3e71cfa4cd4bc0861c811d370fd97f33d8
Todd <todd.griffin61@outlook.com>
2024-03-26 16:19:19 +0000

    added role select

--- src/routes/tickets/tickets.tsx
+++ src/routes/tickets/tickets.tsx
@@ -13,20 +13,38 @@ const filterMatchesName = (searchItem: string, userSearch: string) => {
 
 const Tickets = () => {
   const [searchTerm, setSearchTerm] = useState("")
+  const [roleSearch, setRoleSearch] = useState("")
 
-	const handleSearch = (event: FormEvent) => {
-					const inputTarget = event.currentTarget as HTMLInputElement
-					setSearchTerm(inputTarget.value)
-				}
+  const handleSearch = (event: FormEvent) => {
+    const inputTarget = event.currentTarget as HTMLInputElement
+    setSearchTerm(inputTarget.value)
+  }
+
+  const uniqueRoles = new Set<string>()
+  team.forEach(member => {
+    uniqueRoles.add(member.role)
+  })
+
+  const handleRoleChange = (event: FormEvent) => {
+    const selectTarget = event.currentTarget as HTMLSelectElement
+    setRoleSearch(selectTarget.value)
+  }
 
   return (
-		<>
-			<input type="text" onChange={handleSearch} />
-			<section className="main">
-				{team.filter(({ name }) => filterMatchesName(name, searchTerm))
-					.map(({ name, role, id }) => <Ticket name={name} role={role} key={id} />)}
-			</section>
-		</>
+    <>
+      <section className="search">
+        <input className="search__name-input" type="text" onChange={handleSearch} />
+        <select className="search__role-select" id="role" name="role" onChange={handleRoleChange}>
+          <option value="">All roles</option>
+          {Array.from(uniqueRoles).map(role => <option value={role}>{role}</option>)}
+        </select>
+      </section>
+      <section className="main">
+        {team.filter(({ name }) => filterMatchesName(name, searchTerm))
+          .filter(({ role }) => filterMatchesName(role, roleSearch))
+          .map(({ name, role, id }) => <Ticket name={name} role={role} key={id} />)}
+      </section>
+    </>
   )
 }
 
